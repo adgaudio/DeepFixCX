1d318573767f69942a9ab99ef7afaa793eaaa349
Alex Gaudio <adgaudio@gmail.com>
2022-02-17 17:00:47 +0000

    tweak the plot chexpert distribution script

--- bin/plot_chexpert_class_distribution.py
+++ bin/plot_chexpert_class_distribution.py
@@ -5,30 +5,28 @@ from matplotlib import pyplot as plt
 leaderboard = [
     'Atelectasis', 'Cardiomegaly', 'Consolidation', 'Edema', 'Pleural Effusion']
 
-df = pd.read_csv("./data/CheXpert-v1.0-small/train.csv")
-# add a * for leaderboard classes
-df.columns = [x+("*" if x in leaderboard else "") for x in df.columns]
-z = df[df.columns[-14:]].melt()
-z["count"] = 1
-z = z.groupby(["variable", "value"]).count().unstack("value")[
-    [("count", 0.0), ("count", 1.0)]
-].droplevel(None, axis=1)
-ax = z.plot.barh(stacked=True)
-plt.tight_layout()
-plt.savefig('./results/plots/chexpert_class_distribution_TRAIN.png', bbox_inches='tight')
+
+def make_plot(fp_csv_in, fp_img_out):
+    df = pd.read_csv(fp_csv_in)
+    # add a * for leaderboard classes
+    df.columns = [x+("*" if x in leaderboard else "") for x in df.columns]
+    z = df[df.columns[-14:]].melt(var_name='Diagnostic Class')
+    z["count"] = 1
+    z = (z
+         .replace(-1,2)  # set uncertain values as 2
+         .fillna(-2)  # set missing values as -2
+         .groupby(["Diagnostic Class", "value"]).count().unstack("value") #  [[("count", 0.0), ("count", 1.0)]
+         .droplevel(None, axis=1))
+    cols = {-2: '- (Missing)', 0: '-', 1: '+', 2: 'Uncertain'}
+    z.columns = [cols[k] for k in z.columns]
+    ax = z.plot.barh(stacked=True)
+    plt.tight_layout()
+    plt.savefig(fp_img_out, bbox_inches='tight')
 
 
-
-df = pd.read_csv("./data/CheXpert-v1.0-small/valid.csv")
-df.columns = [x+("*" if x in leaderboard else "") for x in df.columns]
-z = df[df.columns[-14:]].melt()
-z["count"] = 1
-z = (
-    z.groupby(["variable", "value"])
-    .count()
-    .unstack("value")[[("count", 0.0), ("count", 1.0)]]
-    .droplevel(None, axis=1)
-)
-z.plot.barh(stacked=True)
-plt.tight_layout()
-plt.savefig('./results/plots/chexpert_class_distribution_TEST.png', bbox_inches='tight')
+make_plot(
+    './data/CheXpert-v1.0-small/train.csv',
+    './results/plots/chexpert_class_distribution_TRAIN.png')
+make_plot(
+    './data/CheXpert-v1.0-small/valid.csv',
+    './results/plots/chexpert_class_distribution_TEST.png')
