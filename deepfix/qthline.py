02f97c6156e29daed50f4230d4259a49511c7a43
Elvin Johnson <elvinj@br013.ib.bridges2.psc.edu>
2022-04-18 18:15:31 -0400

    first hline commit (shreshta's code)

---
+++ deepfix/qthline.py
@@ -0,0 +1,67 @@
+import torch as T
+import torch.nn as nn
+
+class HLine(T.nn.Module):
+    def __init__(self,lines):
+        super(HLine,self).__init__()
+        self.lines=lines
+    def forward(self, x: T.Tensor):
+        """
+        Args:
+            x:  a minibatch of x-ray images of shape (B,1,H,W)
+        """
+        hlines = x[:,:,self.lines, :]
+        return hlines
+
+
+class QuadTree(T.nn.Module):
+    def __init__(self, threshold):
+        super(Quadtree,self).__init()
+        self.threshold = threshold
+    def forward(self, x):
+        return self.quadtree_compress(x, threshold)
+
+class MlpClassifier(T.nn.Module):
+    def __init__(self, activation, input_size):
+        super(MlpClassifier, self).__init__()
+
+        if activation == 'CELU':
+            activation_fn = nn.CELU()
+        elif activation == 'SELU':
+            activation_fn = nn.SELU()
+        elif activation == 'Tanh':
+            activation_fn = nn.Tanh()
+        elif activation == 'RELU':
+            activation_fn = nn.ReLU()
+            
+        layers = [nn.Flatten()]
+        layers += [nn.Linear(input_size,25600)]
+        layers += [nn.BatchNorm1d(25600)]
+        layers += [activation_fn]        
+        layers += [nn.Linear(300,1)]
+        layers += [nn.Sigmoid()]
+        
+    def forward(self,x):
+        op = self.mlp(x)
+        return op
+
+class QTHlineClassifier(T.nn.Module):
+    def __init__(self,lines,image_size,mlp_activation,threshold):
+        super(QTHlineClassifier,self).__init__()
+
+        self.hline=HLine(lines=lines)
+
+        if threshold is not None:
+            self.quadtree=QuadTree(threshold)
+        else:
+            self.quadtree=None
+
+            
+        mlp_input_size=len(lines)*image_size
+        self.mlp = MlpClassifier(activation=mlp_activation,input_size=mlp_input_size)
+
+    def forward(self, x):
+        if self.quadtree is not None:
+            x = self.quadtree(x)
+        x = self.hline(x)
+        return self.mlp(x)
